da2cd9c50f11276f8c13bba3396f41c67ef65ad1
Lin-yiming <166944133+Lin-yiming@users.noreply.github.com>
2024-04-14 13:46:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -49,11 +49,7 @@ def callback():
 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
-    
+   
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
