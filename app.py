fb30df9508d67af5efcf1237fee7ce3a1670e494
Lin-yiming <166944133+Lin-yiming@users.noreply.github.com>
2024-04-16 03:04:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,75 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  2 21:16:35 2021
 
-@author: Ivan
-版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+import matplotlib.pyplot as plt
+import numpy as np
 
-Line Bot聊天機器人
-第一章 Line Bot申請與串接
-Line Bot機器人串接與測試
-"""
-#載入LineBot所需要的套件
-from flask import Flask, request, abort
+plt.rcParams['font.family'] = 'sans-serif' 
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
+# 設定顏色
+colorcompetitor = ['#d9f776','#76f794','#9476f7','#f776d9','#e70e4b','#76d9f7','#f79476','#fbccbe']
+ 
 
-app = Flask(__name__)
-
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('p7Cmx4BoCNt0LD2kgdfeOe75gPTHF3sLGrR099KNnnrTdJK5RBzaAxB58kQs7XWmOlKesfndO2M6Nl9q4SeYn7+700i3CqocUHqzN+TeBZoCiktCjDL5w9fLfW9ed++jljaF0zYUhp620TxhWDkeTwdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
-handler = WebhookHandler('980186763ec26279c6c95254f44a4ae8')
-
-line_bot_api.push_message('Uae4d95a8996273cbd5fd013544cb3d5a', TextSendMessage(text='你可以開始了'))
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = text=event.message.text
-    if re.match('告訴我秘密',message):
-        flex_message = TextSendMessage(text='以下有雷，請小心',
-                               quick_reply=QuickReply(items=[
-                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                   QuickReplyButton(action=MessageAction(label="別按我", text="你按屁喔！爆炸了拉！！")),
-                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
-                                   QuickReplyButton(action=MessageAction(label="按我", text="按！"))
-                               ]))
-        line_bot_api.reply_message(event.reply_token, flex_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-#主程式
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+searchbrand=['N','S','A','U2','M','T','D','U']
+a=          [0.16,0.13,0.34,0.29,0.4,0.26,0.21,0.13]
+b=          [0.14,0.18,0.16,0.15,0.28,0.14,0.26,0.04]
+area =      [1225,1600,400,400,324,400,400,25]
+sh=plt.scatter(a,b,s=area,c=colorcompetitor,alpha=0.5)
+ax = plt.gca()
+plt.axvline(np.mean(a), color='c', linestyle='dashed', linewidth=1) # 繪製平均線
+plt.axhline(np.mean(b), color='c', linestyle='dashed', linewidth=1) # 繪製平均線
+plt.title("品牌定位圖_舒適&時尚",fontsize=30)
+plt.ylabel('舒適',fontsize=15)
+plt.xlabel('時尚',fontsize=15)
+for j in range(len(searchbrand)):
+    plt.text(a[j], b[j], '%s' % searchbrand[j], fontsize=10 )
