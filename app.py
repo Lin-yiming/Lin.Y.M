9a1aecbd1125a1a7a7d152184ed07744014e9220
Lin-yiming <166944133+Lin-yiming@users.noreply.github.com>
2024-04-16 03:37:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,68 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun  3 21:37:37 2021
+Created on Wed Jun  2 21:16:35 2021
 
 @author: Ivan
 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
 
 Line Bot聊天機器人
 第三章 互動回傳功能
-推播push_message與回覆reply_message
+傳送貼圖StickerSendMessage
 """
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-import time
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import re
+app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('p7Cmx4BoCNt0LD2kgdfeOe75gPTHF3sLGrR099KNnnrTdJK5RBzaAxB58kQs7XWmOlKesfndO2M6Nl9q4SeYn7+700i3CqocUHqzN+TeBZoCiktCjDL5w9fLfW9ed++jljaF0zYUhp620TxhWDkeTwdB04t89/1O/w1cDnyilFU=')
-# 請填入您的ID
-yourID = 'Uae4d95a8996273cbd5fd013544cb3d5a'
-# 主動推播訊息
-line_bot_api.push_message(yourID, 
-                          TextSendMessage(text='安安您好！早餐吃了嗎？'))
-# 用迴圈推播訊息
-for i in [1,2,3,4,5]:
-    line_bot_api.push_message(yourID, 
-                              TextSendMessage(text='我們來倒數：'+str(i)))
-    time.sleep(1)
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('980186763ec26279c6c95254f44a4ae8')
+
+line_bot_api.push_message('Uae4d95a8996273cbd5fd013544cb3d5a', TextSendMessage(text='你可以開始了'))
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        # 貼圖查詢：https://developers.line.biz/en/docs/messaging-api/sticker-list/#specify-sticker-in-message-object
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+        line_bot_api.reply_message(event.reply_token, sticker_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
