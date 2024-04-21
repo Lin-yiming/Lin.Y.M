ebe41f6f2697fa2fd4a3897b0ef7682b84d18221
Lin-yiming <166944133+Lin-yiming@users.noreply.github.com>
2024-04-21 08:55:30 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# imgur key
+client_id = 'd6f8a94c37e2b83'
+client_secret = 'e63b686c655b40cc16e0bebad71dac82e10ec54e'
+album_id = 'https://imgur.com/a/COUVmDj'
+access_token = 'e34c39966b38a096aabe0f004e553c14b2ad9f09'
+refresh_token = 'c9a521ccb443ef53eaa5faa2a8b5b79bd24c6a24'
+
+# line bot key
+line_channel_access_token = 'p7Cmx4BoCNt0LD2kgdfeOe75gPTHF3sLGrR099KNnnrTdJK5RBzaAxB58kQs7XWmOlKesfndO2M6Nl9q4SeYn7+700i3CqocUHqzN+TeBZoCiktCjDL5w9fLfW9ed++jljaF0zYUhp620TxhWDkeTwdB04t89/1O/w1cDnyilFU='
+line_channel_secret = '980186763ec26279c6c95254f44a4ae8'
