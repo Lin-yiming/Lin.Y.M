3e7467cdca6ff303e436a246f363b3d2b8163065
Lin-yiming <166944133+Lin-yiming@users.noreply.github.com>
2024-04-21 09:05:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,105 +1,35 @@
-import random
-from flask import Flask, request, abort
 from imgurpython import ImgurClient
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-import tempfile, os
-from config import client_id, client_secret, album_id, access_token, refresh_token, line_channel_access_token, \
-    line_channel_secret
-
-app = Flask(__name__)
-
-line_bot_api = LineBotApi(line_channel_access_token)
-handler = WebhookHandler(line_channel_secret)
-
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+from config import client_id, client_secret
 
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    # print("body:",body)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'ok'
+def get_input(string):
+    return input(string)
 
 
-@handler.add(MessageEvent, message=(ImageMessage, TextMessage))
-def handle_message(event):
-    if isinstance(event.message, ImageMessage):
-        ext = 'jpg'
-        message_content = line_bot_api.get_message_content(event.message.id)
-        with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
-            for chunk in message_content.iter_content():
-                tf.write(chunk)
-            tempfile_path = tf.name
+def authenticate():
+    # Get client ID and secret from config.py
 
-        dist_path = tempfile_path + '.' + ext
-        dist_name = os.path.basename(dist_path)
-        os.rename(tempfile_path, dist_path)
-        try:
-            client = ImgurClient(client_id, client_secret, access_token, refresh_token)
-            config = {
-                'album': album_id,
-                'name': 'Catastrophe!',
-                'title': 'Catastrophe!',
-                'description': 'Cute kitten being cute on '
-            }
-            path = os.path.join('static', 'tmp', dist_name)
-            client.upload_from_path(path, config=config, anon=False)
-            os.remove(path)
-            print(path)
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='上傳成功'))
-        except:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='上傳失敗'))
-        return 0
+    client = ImgurClient(client_id, client_secret)
 
-    elif isinstance(event.message, VideoMessage):
-        ext = 'mp4'
-    elif isinstance(event.message, AudioMessage):
-        ext = 'm4a'
-    elif isinstance(event.message, TextMessage):
-        if event.message.text == "看看大家都傳了什麼圖片":
-            client = ImgurClient(client_id, client_secret)
-            images = client.get_album_images(album_id)
-            index = random.randint(0, len(images) - 1)
-            url = images[index].link
-            image_message = ImageSendMessage(
-                original_content_url=url,
-                preview_image_url=url
-            )
-            line_bot_api.reply_message(
-                event.reply_token, image_message)
-            return 0
-        else:
-            line_bot_api.reply_message(
-                event.reply_token, [
-                    TextSendMessage(text=' yoyo'),
-                    TextSendMessage(text='請傳一張圖片給我')
-                ])
-            return 0
+    # Authorization flow, pin example (see docs for other auth types)
+    authorization_url = client.get_auth_url('pin')
+
+    print("Go to the following URL: {0}".format(authorization_url))
+
+    # Read in the pin, handle Python 2 or 3 here.
+    pin = get_input("Enter pin code: ")
+
+    # ... redirect user to `authorization_url`, obtain pin (or code or token) ...
+    credentials = client.authorize(pin, 'pin')
+    client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
+
+    print("Authentication successful! Here are the details:")
+    print("   Access token:  {0}".format(credentials['access_token']))
+    print("   Refresh token: {0}".format(credentials['refresh_token']))
+
+    return client
 
 
-if __name__ == '__main__':
-    app.run()
+# If you want to run this as a standalone script, so be it!
+if __name__ == "__main__":
+    authenticate()
